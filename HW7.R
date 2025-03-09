b820aeaea8de7d5a8ec8dc69953ee60da250c362
Jackson Colby <jcolby@colgate.edu>
2025-03-09 16:34:02 -0400

    Question 2 Finished

--- HW7.R
+++ HW7.R
@@ -24,3 +24,47 @@ pois.prob(x=4, lambda = 2, type = "<")  # Probability that X < 4
 
 #Part 2
 
+beta.prob <- function(x, alpha, beta, type="<=") {
+  
+  # Calculate the probability based on the specified type
+  if (type == "=") {
+    # P(X = x) is the PDF at x, which is 0 since beta distribution is continuous
+    return(0)
+  }
+  else if (type == "<") {
+    # P(X < x) is the CDF at x P(X=x)=0 so "<" = "<="
+    return(pbeta(x, shape1 = alpha, shape2 = beta))
+  }
+  else if (type == "<=") {
+    # P(X <= x) is the CDF at x
+    return(pbeta(x, shape1 = alpha, shape2 = beta))
+  }
+  else if (type == ">") {
+    # P(X > x) is 1 minus the CDF at x
+    return(1 - pbeta(x, shape1 = alpha, shape2 = beta))
+  }
+  else if (type == ">=") {
+    # P(X >= x) is 1 minus the CDF at x since ">=" and ">" are equal
+    return(1 - pbeta(x, shape1 = alpha, shape2 = beta))
+  }
+}
+
+# Example usage of beta.prob function
+alpha <- 2
+beta <- 3
+x <- 0.5
+
+# P(X = x)
+print(beta.prob(x, alpha, beta, "="))
+
+# P(X < x)
+print(beta.prob(x, alpha, beta, "<"))
+
+# P(X <= x)
+print(beta.prob(x, alpha, beta, "<="))
+
+# P(X > x)
+print(beta.prob(x, alpha, beta, ">"))
+
+# P(X >= x)
+print(beta.prob(x, alpha, beta, ">="))
